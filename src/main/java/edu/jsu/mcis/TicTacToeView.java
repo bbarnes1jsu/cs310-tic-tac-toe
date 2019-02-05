051dcb769f4f4d7436865daab8746315de4a7a30
Brandon Barnes <bbarnes1.stu.jsu.edu>
2019-02-05 13:40:27 -0600

    Changes to view

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -22,21 +22,29 @@ public class TicTacToeView {
         /* Prompt the player to enter the row and the column of their next move.
            Return as a TicTacToeMove object. */
 
-        /*if(isXTurn()){
-            System.out.println("Player 1 (X) Move: ");
+        int row;
+        int col;
+
+        if(isXTurn){
+            System.out.println("Player 1 (X) Move" + "\n\n" + "Enter the row and the column numbers, speperated by a space: ");
+            row = Integer.parseInt(keyboard.next()); //Retrieves next Keyboard Int Input and stores it
+            col = Integer.parseInt(keyboard.next()); //Retrieves next Keyboard Int Input and stores it
+
+            TicTacToeMove move = new TicTacToeMove(row,col);
+
+            return move;
         }
         else{
-            System.out.println("Player 2 (O) Move: ");
-        } */
+            System.out.println("Player 2 (O) Move"  + "\n\n" + "Enter the row and the column numbers, speperated by a space: ");
+            row = Integer.parseInt(keyboard.next()); //Retrieves next Keyboard Int Input and stores it
+            col = Integer.parseInt(keyboard.next()); //Retrieves next Keyboard Int Input and stores it
+
+            TicTacToeMove move = new TicTacToeMove(row,col);
+
+            return move;
+        } 
+
         
-        System.out.println("Enter the row and the column numbers, speperated by a space: ");
-
-        int row = Integer.parseInt(keyboard.next()); //Retrieves next Keyboard Int Input and stores it
-        int col = Integer.parseInt(keyboard.next()); //Retrieves next Keyboard Int Input and stores it
-
-        TicTacToeMove move = new TicTacToeMove(row,col);
-
-        return move;
 
     }
 
